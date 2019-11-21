5cfd4b2d55e7e85cf0943ac1ee8dcfe82f894298
DanEscudero <daniel.escudero05@gmail.com>
2019-11-21 09:10:00 -0300

    Add lab 17

---
+++ lab17/main.cpp
@@ -0,0 +1,277 @@
+#define GLEW_STATIC
+#define GLFW_INCLUDE_NONE
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow *window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include "common/shader.cpp"
+#include "common/texture.cpp"
+
+glm::mat4 getRotation(char axis, float ang)
+{
+	switch (axis)
+	{
+	case 'x':
+		return {
+			{1, 0, 0, 0},
+			{0, cos(ang), -sin(ang), 0},
+			{0, sin(ang), cos(ang), 0},
+			{0, 0, 0, 1}};
+
+	case 'y':
+		return {
+			{cos(ang), 0, sin(ang), 0},
+			{0, 1, 0, 0},
+			{-sin(ang), 0, cos(ang), 0},
+			{0, 0, 0, 1}};
+
+	case 'z':
+		return {
+			{cos(ang), -sin(ang), 0, 0},
+			{sin(ang), cos(ang), 0, 0},
+			{0, 0, 1, 0},
+			{0, 0, 0, 1}};
+	}
+}
+
+int main(void)
+{
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Lab 17", NULL, NULL);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("shader/TransformVertexShader.sdr", "shader/TextureFragmentShader.sdr");
+
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Get a handle for our buffers
+	GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
+	GLuint vertexUVID = glGetAttribLocation(programID, "vertexUV");
+
+	// Projection matrix : 45 Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	// Load the texture using any two methods
+	GLuint texture = loadBMP_custom("uvtemplate.bmp");
+
+	// Get a handle for our "myTextureSampler" uniform
+	GLuint textureID = glGetUniformLocation(programID, "myTextureSampler");
+
+	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	const float size = 1.0;
+	static const GLfloat vertexes[] = {
+		-size, -size, -size,
+		-size, -size, +size,
+		-size, +size, +size,
+
+		+size, +size, -size,
+		-size, -size, -size,
+		-size, +size, -size,
+
+		+size, -size, +size,
+		-size, -size, -size,
+		+size, -size, -size,
+
+		+size, +size, -size,
+		+size, -size, -size,
+		-size, -size, -size,
+
+		-size, -size, -size,
+		-size, +size, +size,
+		-size, +size, -size,
+
+		+size, -size, +size,
+		-size, -size, +size,
+		-size, -size, -size,
+
+		-size, +size, +size,
+		-size, -size, +size,
+		+size, -size, +size,
+
+		+size, +size, +size,
+		+size, -size, -size,
+		+size, +size, -size,
+
+		+size, -size, -size,
+		+size, +size, +size,
+		+size, -size, +size,
+
+		+size, +size, +size,
+		+size, +size, -size,
+		-size, +size, -size,
+
+		+size, +size, +size,
+		-size, +size, -size,
+		-size, +size, +size,
+
+		+size, +size, +size,
+		-size, +size, +size,
+		+size, -size, +size};
+
+	// Two UV coordinates for each vertex. They were created with Blender.
+	static const GLfloat uv_coordinates[] = {
+		0.000059f, 1.0f - 0.000004f,
+		0.000103f, 1.0f - 0.336048f,
+		0.335973f, 1.0f - 0.335903f,
+		1.000023f, 1.0f - 0.000013f,
+		0.667979f, 1.0f - 0.335851f,
+		0.999958f, 1.0f - 0.336064f,
+		0.667979f, 1.0f - 0.335851f,
+		0.336024f, 1.0f - 0.671877f,
+		0.667969f, 1.0f - 0.671889f,
+		1.000023f, 1.0f - 0.000013f,
+		0.668104f, 1.0f - 0.000013f,
+		0.667979f, 1.0f - 0.335851f,
+		0.000059f, 1.0f - 0.000004f,
+		0.335973f, 1.0f - 0.335903f,
+		0.336098f, 1.0f - 0.000071f,
+		0.667979f, 1.0f - 0.335851f,
+		0.335973f, 1.0f - 0.335903f,
+		0.336024f, 1.0f - 0.671877f,
+		1.000004f, 1.0f - 0.671847f,
+		0.999958f, 1.0f - 0.336064f,
+		0.667979f, 1.0f - 0.335851f,
+		0.668104f, 1.0f - 0.000013f,
+		0.335973f, 1.0f - 0.335903f,
+		0.667979f, 1.0f - 0.335851f,
+		0.335973f, 1.0f - 0.335903f,
+		0.668104f, 1.0f - 0.000013f,
+		0.336098f, 1.0f - 0.000071f,
+		0.000103f, 1.0f - 0.336048f,
+		0.000004f, 1.0f - 0.671870f,
+		0.336024f, 1.0f - 0.671877f,
+		0.000103f, 1.0f - 0.336048f,
+		0.336024f, 1.0f - 0.671877f,
+		0.335973f, 1.0f - 0.335903f,
+		0.667969f, 1.0f - 0.671889f,
+		1.000004f, 1.0f - 0.671847f,
+		0.667979f, 1.0f - 0.335851f};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
+
+	GLuint uvbuffer;
+	glGenBuffers(1, &uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(uv_coordinates), uv_coordinates, GL_STATIC_DRAW);
+
+	do
+	{
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// Send our transformation to the currently bound shader, in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		// Bind our texture in Texture Unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		// Set our "myTextureSampler" sampler to user Texture Unit 0
+		glUniform1i(textureID, 0);
+
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(vertexPosition_modelspaceID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(vertexPosition_modelspaceID, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+		// 2nd attribute buffer : UVs
+		glEnableVertexAttribArray(vertexUVID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+		glVertexAttribPointer(vertexUVID, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+		// Draw the triangles
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+
+		glDisableVertexAttribArray(vertexPosition_modelspaceID);
+		glDisableVertexAttribArray(vertexUVID);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		// Rotate
+		MVP *= getRotation('x', 0.02);
+		MVP *= getRotation('y', 0.015);
+		MVP *= getRotation('z', 0.01);
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0);
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &uvbuffer);
+	glDeleteProgram(programID);
+	glDeleteTextures(1, &textureID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
