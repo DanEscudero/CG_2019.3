3a3511bba95eacbc0808db29b927131bbf3b473d
DanEscudero <daniel.escudero05@gmail.com>
2019-11-25 20:23:25 -0300

    minor

--- lab15/main.cpp
+++ lab15/main.cpp
@@ -23,10 +23,12 @@ using namespace std;
 
 #include "common/shader.cpp"
 
+// Auxiliares para controlar input do teclado
 double currentTime = 0;
 double keyPressDelay = 0.015;
 double lastPressedKey = 0;
 
+// Matrizes de transformação
 glm::mat4 scaleMatrix = glm::mat4(1.0f);
 glm::mat4 rotationMatrix = glm::mat4(1.0f);
 glm::mat4 translationMatrix = glm::mat4(1.0f);
@@ -152,7 +154,6 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 	lastPressedKey = currentTime;
 
-	cout << key << endl;
 	if (key == GLFW_KEY_X)
 		rotate('x');
 
@@ -181,6 +182,20 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 		scale(-1);
 }
 
+void print(glm::mat4 m)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << m[i][j] << ' ';
+		}
+		cout << ' ' << endl;
+	}
+
+	cout << "=========" << endl;
+}
+
 int main(void)
 {
 	// Initialise GLFW
@@ -198,7 +213,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "LAB 15", NULL, NULL);
+	window = glfwCreateWindow(512, 384, "LAB 15", NULL, NULL);
 	if (window == NULL)
 	{
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
@@ -240,13 +255,15 @@ int main(void)
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
 	// Projection matrix : 45 Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 0.2f, 0.1f, 100.0f);
+
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
 		glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
 		glm::vec3(0, 0, 0),  // and looks at the origin
 		glm::vec3(0, 1, 0)   // Head is up (set to 0,-1,0 to look upside-down)
 	);
+
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 Model = glm::mat4(1.0f);
 
@@ -301,11 +318,11 @@ int main(void)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-		Model = Model * scaleMatrix;
-		Model = Model * rotationMatrix;
-		Model = Model * translationMatrix;
+		Model *= translationMatrix;
+		Model *= rotationMatrix;
+		Model *= scaleMatrix;
 
-		glm::mat4 MVP = Projection * View * Model;
+		MVP = Projection * View * Model;
 
 		// Reset translation and scale matrix
 		// Rotation shouldn't be reset, because it's state is controlled with rotationState
